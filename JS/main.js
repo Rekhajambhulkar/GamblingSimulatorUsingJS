a65b6e88ce0ca7cbfefc298108b714fe23bb3e88
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2021-01-18 22:58:38 +0530

    Check either its win or lose

--- JS/main.js
+++ JS/main.js
@@ -2,3 +2,16 @@ console.log("WElcome to Gambling Simulator program")
 
 const bet = 1;
 const stake = 100;
+
+winOrLose = () => {
+    let win = 1;
+    let random = Math.floor(Math.random() * 10) % 2;
+    if (win == random){
+        console.log("Win")
+    }else{
+        console.log("Lose")
+    }
+}
+winOrLose();
+
+module.export = {winOrLose};
